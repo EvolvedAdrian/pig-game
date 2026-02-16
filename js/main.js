631401f1fe7586a9dff30c07e786c4937e6e29d1
HelloAdrian2k <mz.adriaan@gmail.com>
2026-02-17 00:31:32 +0100

    feat: add player current score functionality
    * Refactor player score variables following DRY principles

--- js/main.js
+++ js/main.js
@@ -6,41 +6,48 @@ const rollDiceButton = document.querySelector('.button__roll-dice');
 const dice = document.querySelector('.dice-img');
 
 // Player scores
-let player1GlobalScore = 0;
-let player1CurrentScore = 0;
-let player2GlobalScore = 0;
-let player2CurrentScore = 0;
+let currentScore = 0;
+let scores = [0, 0];
 
 let activePlayer = 0;
 let gameIsActive = true;
 
-// Generates dice random number from 1 to 6
-function generateDiceNumber(){
+// Generate a random dice number from 1 to 6
+function generateRandomDiceNumber() {
     return Math.ceil(Math.random() * 6);
 }
 
 // Activate active and non active player sytiling
-function setPlayerStyles(activePlayer){
-    document.querySelector(`.player--${activePlayer+1}`).dataset.active = 'true';
-    document.querySelector(`.player--${!activePlayer+1}`).dataset.active = 'false';
+function setPlayerStyles(activePlayer) {
+    document.querySelector(`.player--${activePlayer + 1}`).dataset.active = 'true';
+    document.querySelector(`.player--${!activePlayer + 1}`).dataset.active = 'false';
 }
 
 // Change player
-function changePlayer(){
-    activePlayer = !activePlayer;
+function changePlayer() {
+    // Set last current player score to 0 (HTML)
+    document.querySelector(`.player__current-score--${activePlayer + 1}`).textContent = 0;
+    
+    // Change to new player
+    activePlayer = Number(!activePlayer);
     setPlayerStyles(activePlayer);
+    currentScore = 0;
 }
 
-// Set current player 
-
 // Game logic
+
 // Roll dice logic
-rollDiceButton.addEventListener('click',() => {
+rollDiceButton.addEventListener('click', () => {
     // Throw the dice
-    const randomDiceNumber = generateDiceNumber()
+    const randomDiceNumber = generateRandomDiceNumber()
     dice.setAttribute('srcset', `./assets/img/dice-${randomDiceNumber}/dice-${randomDiceNumber}-300.webp 300w`);
     dice.setAttribute('src', `./assets/img/dice-${randomDiceNumber}/dice-${randomDiceNumber}-300.png`);
-    
-    
-    
+
+    // Increase active player current score if dice number is NOT 1
+    if (randomDiceNumber !== 1) {
+        currentScore += randomDiceNumber;
+        document.querySelector(`.player__current-score--${activePlayer + 1}`).textContent = currentScore;
+    } else {
+        changePlayer();
+    }
 });
